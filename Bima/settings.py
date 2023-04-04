bf1bb56cf3a6645d1e3040524b17e28317f7ac2f
Manjeet Nandal <Manjeet Nandal@DESKTOP-GL7QKAG>
2023-04-04 23:06:36 +0530

    storage added boto3

--- Bima/settings.py
+++ Bima/settings.py
@@ -157,7 +157,7 @@ DATA_UPLOAD_MAX_NUMBER_FIELDS = None
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
 AWS_ACCESS_KEY_ID = "AKIAQHVWIUX56OGJLSBN"
